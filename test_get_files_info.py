682380a00331c14ca38842158fa3103e42de744c
Michele Coppola <michele.coppola17@outlook.it>
2026-03-12 11:21:34 +0100

    add read file abilities

--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,26 +1,23 @@
 from functions.get_files_info import get_files_info
 
-def main():
-    print("get_files_info(\"calculator\", \".\"):")
+def test():
+    result = get_files_info("calculator", ".")
     print("Result for current directory:")
-    result1 = get_files_info("calculator", ".")
-    print("  " + result1.replace("\n", "\n  "))
+    print(result)
+    print("")
 
-    print("\nget_files_info(\"calculator\", \"pkg\"):")
+    result = get_files_info("calculator", "pkg")
     print("Result for 'pkg' directory:")
-    result2 = get_files_info("calculator", "pkg")
-    print("  " + result2.replace("\n", "\n  "))
+    print(result)
 
-    print("\nget_files_info(\"calculator\", \"/bin\"):")
+    result = get_files_info("calculator", "/bin")
     print("Result for '/bin' directory:")
-    result3 = get_files_info("calculator", "/bin")
-    print("    " + result3.replace("\n", "\n    "))
+    print(result)
 
-    print("\nget_files_info(\"calculator\", \"../\"):")
+    result = get_files_info("calculator", "../")
     print("Result for '../' directory:")
-    result4 = get_files_info("calculator", "../")
-    print("    " + result4.replace("\n", "\n    "))
+    print(result)
 
 
 if __name__ == "__main__":
-    main()
+    test()
